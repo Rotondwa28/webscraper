e3428762c09757b75c785d4c6d77882536a2ed56
NEMALILI <NEMALILI@ROTONDWA>
2023-01-17 22:32:55 +0200

    Add project files.

---
+++ webscraper/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+        HtmlAgilityPack.HtmlDocument doc = web.Load("https://app.slack.com/client/T02EW3SC4/C02EW4SDY");
+        foreach (var item in doc.DocumentNode.SelectNodes("feedback"))
+        {
+            Console.WriteLine(item.InnerText);
+        }
+    }
+}
